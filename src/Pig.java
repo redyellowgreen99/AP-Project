31f0bc7c1ca4dcdc4e626b3ead0c2dd5781a13a2
Alex <Alex@10.0.1.14>
2013-03-30 14:44:46 -0700

    -Added Pig Colors -Added getters -Added PigStats -Removed Herobrine

--- src/Pig.java
+++ src/Pig.java
@@ -1,21 +1,36 @@
 public class Pig {
 
 	int numberOfBaconSlices; //Create an int
+	String color;
+	String name;
 
 	public static void main(String[] args) {
 
 	}
 
-	public Pig(int numberOfBaconSlices, String name) { //Pig constructor
+	public Pig(int numberOfBaconSlices, String name, String color) { //Pig constructor
 		this.numberOfBaconSlices = numberOfBaconSlices; //Set the ints to each other
+		this.color = color;
+		this.name = name;
+	}
+	
+	public String getName() { //Create a getter
+		return name;
 	}
 
 	public int getBaconSlices() { //Create a getter
 		return numberOfBaconSlices; //Return the number of bacon slices to the other class
 	}
 	
+	public String getColor() { //Create a getter
+		return color; //Return the color to the other class
+	}
+	
 	public void setBaconSlices(int setTo) { //Create a setter
 		numberOfBaconSlices = setTo; //set to the parameter's int
 	}
 
+	public void setColor(String setTo) { //Create a setter
+		color = setTo; //set to the parameter's int
+	}
 }
--- src/pigArray.java
+++ src/pigArray.java
@@ -4,14 +4,15 @@ import java.util.ArrayList;
 public class pigArray {
 
 	public static void main(String[] args) {
-		Pig pig = new Pig(32, "firstPig"); // Add and declare a pig
+		Pig pig = new Pig(32, "firstPig", "pink"); // Add and declare a pig
 		ArrayList<Pig> PigList = new ArrayList<Pig>(); // Create an ArrayList
 		PigList.add(pig); // Add the pig to the ArrayList
-		PigList.add(new Pig(12, "secondPig")); // Add new pigs to the ArrayList
-		PigList.add(new Pig(36, "thirdPig"));
-		PigList.add(new Pig(18, "forthPig"));
+		PigList.add(new Pig(12, "secondPig", "dark pink")); // Add new pigs to the ArrayList
+		PigList.add(new Pig(36, "thirdPig", "unsaturated pink"));
+		PigList.add(new Pig(18, "forthPig", "extra contrast pink"));
 		getBaconSlices(PigList); // Run getBaconSlices
 		harvestBacon(PigList); // Run harvestBacon
+		getStats(pig);
 	}
 
 	public static int getBaconSlices(ArrayList<Pig> PigList) {
@@ -23,6 +24,23 @@ public class pigArray {
 		System.out.println("Total Number Of Bacon Slices: "
 				+ totalNumberOfBaconSlices); // Print out the bacon slices
 		return totalNumberOfBaconSlices; // Return the bacon slices
+	}
+	
+	public static void getStats(Pig pig) {
+		System.out.println("Pig:\n" + pig.getName() + "\nBaconslices:\n" + pig.getBaconSlices() + "\nColor:\n" + pig.getColor());
+	}
+	
+	public static String getColor(Pig pig) {
+		System.out.println("The pig, " + pig + " is the color " + pig.getColor());
+		return pig.getColor();
+	}
+	
+	public static int getNumberColors(ArrayList<Pig> PigList) {
+		int totalColors = 0;
+		for (Pig p : PigList) {
+			totalColors = PigList.size();
+		}
+		return totalColors;
 	}
 
 	public static void harvestBacon(ArrayList<Pig> PigList) {
